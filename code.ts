2555bc9a1c3a8382594ff22969e18be7562e3928
fnaufalh <fnaufalh@gmail.com>
2023-03-20 00:34:34 +0700

    test passed, ready to publish

--- code.ts
+++ code.ts
@@ -1,21 +1,62 @@
-// This plugin creates 5 rectangles on the screen.
-const numberOfRectangles = 5
+const root = figma.root;
+const components: any = [];
+const regex = "components";
+let currentPage = figma.currentPage;
+let select = figma.currentPage.selection;
+let newPage: any = null;
+let orderCount = 0;
 
-// This file holds the main code for the plugins. It has access to the *document*.
-// You can access browser APIs such as the network by creating a UI which contains
-// a full browser environment (see documentation).
-
-const nodes: SceneNode[] = [];
-for (let i = 0; i < numberOfRectangles; i++) {
-  const rect = figma.createRectangle();
-  rect.x = i * 150;
-  rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
-  figma.currentPage.appendChild(rect);
-  nodes.push(rect);
+function moveIt(components: any, componentPage: any) {
+  if (components.length === 0) {
+    figma.closePlugin("There is no component, so clean!");
+    return;
+  }
+  components.sort((a: any, b: any) => a.name.localeCompare(b.name));
+  select = components;
+  for (const item of select) {
+    componentPage.appendChild(item);
+  }
+  figma.currentPage = newPage;
+  for (const [index, value] of components.entries()) {
+    orderCount += index + value.width + (index > 0 ? 200 : 0);
+    const order = { x: orderCount, y: 0 };
+    value.x = order.x;
+    value.y = order.y;
+  }
+  figma.viewport.scrollAndZoomIntoView(components);
 }
-figma.currentPage.selection = nodes;
-figma.viewport.scrollAndZoomIntoView(nodes);
 
-// Make sure to close the plugin when you're done. Otherwise the plugin will
-// keep running, which shows the cancel button at the bottom of the screen.
-figma.closePlugin();
+function componentMover() {
+  const findComponents = Array.from(
+    currentPage.findAll(
+      (node) =>
+        node.type === "COMPONENT_SET" ||
+        (node.type === "COMPONENT" && node.parent?.type !== "COMPONENT_SET")
+    )
+  );
+  for (const component of findComponents) {
+    if (component.parent?.type === "FRAME") {
+      let frame = component.parent as FrameNode;
+      let componentNode = component as ComponentNode;
+      let newInstance = componentNode.createInstance();
+      newInstance.x = component.x;
+      newInstance.y = component.y;
+      frame.insertChild(frame.children.indexOf(component), newInstance);
+    }
+    components.push(component);
+  }
+
+  let findPage = root.findChild(
+    (node) => node.type === "PAGE" && node.name.toLowerCase().includes(regex)
+  ) as PageNode;
+  if (findPage == null) {
+    newPage = figma.createPage();
+    newPage.name = "Components";
+  } else {
+    newPage = findPage;
+  }
+  moveIt(components, newPage);
+  figma.closePlugin("Moving success! Thank you for using me.");
+}
+
+componentMover();
